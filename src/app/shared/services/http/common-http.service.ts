e5f289d5e6b385210e5da786296b0c9dc0a2a4ab
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-01-21 12:17:31 +0530

    Implemented POST PATCH DELETE AND PUT

--- src/app/shared/services/http/common-http.service.ts
+++ src/app/shared/services/http/common-http.service.ts
@@ -1,10 +1,11 @@
 // Angular Modules
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpParams, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { map, catchError, retry } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { PROPERTIES } from '../../constants/app-properties';
 import { errors } from '../../constants/errors';
+import { Observable } from 'rxjs';
 
 // Custom Enums
 
@@ -14,15 +15,16 @@ import { errors } from '../../constants/errors';
 export class CommonHttpService {
 
     private base_url = PROPERTIES.BASE_URL;
-    constructor(private http: HttpClient) {}
+    constructor(private _http: HttpClient) { }
 
     // @method get
     // @params HttpParams if needed
     // @path Request URL
-    public get(path, params: HttpParams = new HttpParams()) {
+    public get(path, params: HttpParams = new HttpParams(), customHeaders?: HttpHeaders): Observable<any> {
         const requestUrl = this.base_url + path;
-        return this.http.get(requestUrl, {
-            params: params
+        return this._http.get(requestUrl, {
+            params: params,
+            headers: customHeaders
         }).pipe(
             map(res => {
                 return res;
@@ -30,6 +32,70 @@ export class CommonHttpService {
             catchError(this.errorHandler), // Catch Errors if service fails
             retry(PROPERTIES.RETRY_SERVICE_COUNT) // if you want to retry the request. Please mention the retry count value
         );
+    }
+    // Utility method for Create.
+    // @method post
+    // @param path
+    // @param body
+    public put(path: string, body: Object = {}, customHeaders?: HttpHeaders): Observable<any> {
+        path = this.base_url + path;
+        const requestOptions = customHeaders ? { headers: customHeaders } : {};
+        return this._http.put(
+            path, body, requestOptions).pipe(
+                map(res => {
+                    return res;
+                }),
+                catchError(this.errorHandler)
+            );
+    }
+    // Utility method for Create.
+    // @method post
+    // @param path
+    // @param body
+    public post(path: string, body: Object = {}, customHeaders?: HttpHeaders): Observable<any> {
+        path = this.base_url + path;
+        const requestOptions = customHeaders ? { headers: customHeaders } : {};
+        return this._http.post(
+            path, body, requestOptions).pipe(
+                map(res => {
+                    return res;
+                }),
+                catchError(this.errorHandler)
+            );
+    }
+
+
+    // Utility method for Create.
+    // @method patch
+    // @param path
+    // @param body
+    // @param customHeaders
+    public patch(path: string, body: Object = {}, customHeaders?: HttpHeaders): Observable<any> {
+        path = this.base_url + path;
+        const requestOptions = customHeaders ? { headers: customHeaders } : {};
+        return this._http.patch(
+            path, body, requestOptions).pipe(
+                map(res => {
+                    return res;
+                }),
+                catchError(this.errorHandler)
+            );
+    }
+
+    // Utility method for Create.
+    // @method delete
+    // @param path
+    // @param customHeaders
+    public delete(path: string, customHeaders?: HttpHeaders): Observable<any> {
+        path = this.base_url + path;
+        const requestOptions = customHeaders ? { headers: customHeaders } : {};
+        return this._http.delete(
+            path, requestOptions).pipe(
+                map(res => {
+                    return res;
+                }),
+                catchError(this.errorHandler)
+            );
     }
 
     // Please add errors and error codes in errors.json.ts file
