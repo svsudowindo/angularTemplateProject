d0223f3274f5302221e00a0b1b9145fd3db7f069
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-01-24 21:31:51 +0530

    Tested with dummy URLS

--- src/app/views/auth-views/login/login.component.ts
+++ src/app/views/auth-views/login/login.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { BaseClass } from './../../../shared/services/common/baseClass';
 import { AppDialogService } from './../../../shared/components/componentsAsService/app-dialog/app-dialog.service';
 import { AppConfirmService } from './../../../shared/components/componentsAsService/app-confirm/app-confirm.service';
+import { CommonHttpService } from '../../../shared/services/http/common-http.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -15,8 +17,11 @@ export class LoginComponent extends BaseClass implements OnInit {
   public errorMessageStatus: string;
 
   // once successfull login make can activate service to true
-  constructor(public route: Router, public injector: Injector,
-    public dialog: AppDialogService, public confirm: AppConfirmService) {
+  constructor(public route: Router,
+    public injector: Injector,
+    public dialog: AppDialogService,
+    public confirm: AppConfirmService,
+    private _httpCommonService: CommonHttpService) {
     super(injector);
   }
 
@@ -37,7 +42,7 @@ export class LoginComponent extends BaseClass implements OnInit {
 
   openConfirm() {
     this.confirm.confirm('title', 'Enter the confirm message', 'Ok', 'Cancel').subscribe((data) => {
-     // alert(JSON.stringify(data));
+      // alert(JSON.stringify(data));
       if (data) {
         this.successMessageStatus = 'Success';
       } else {
@@ -46,4 +51,80 @@ export class LoginComponent extends BaseClass implements OnInit {
     });
   }
 
+  getRemoteData() {
+    this._httpCommonService.get('todos').subscribe((res) => {
+      console.log(res);
+      this.successMessageStatus = 'Success';
+
+    },
+      ((err) => {
+        this.errorMessageStatus = err;
+      }));
+  }
+
+  postRemoteData() {
+    const postBody = JSON.stringify({
+      title: 'sai',
+      body: 'kumar',
+      userId: 999
+    });
+    const headers = new HttpHeaders();
+    headers.set('Content-type', 'application/json; charset=UTF-8');
+    this._httpCommonService.post('posts', postBody, headers).subscribe((res) => {
+      console.log(res);
+      this.successMessageStatus = 'Success';
+
+    },
+      ((err) => {
+        this.errorMessageStatus = err;
+      }));
+  }
+
+
+  putRemoteData() {
+    const postBody = JSON.stringify({
+      title: 'sai1234',
+      body: 'kumar',
+      userId: 999,
+      id: 1
+    });
+    const headers = new HttpHeaders();
+    headers.set('Content-type', 'application/json; charset=UTF-8');
+    this._httpCommonService.put('posts/1', postBody, headers).subscribe((res) => {
+      console.log(res);
+      this.successMessageStatus = 'Success';
+
+    },
+      ((err) => {
+        this.errorMessageStatus = err;
+      }));
+  }
+
+  patchRemoteData() {
+    const postBody = JSON.stringify({
+      title: 'sai1234',
+      body: 'kumar1234'
+    });
+    const headers = new HttpHeaders();
+    headers.set('Content-type', 'application/json; charset=UTF-8');
+    this._httpCommonService.patch('posts/1', postBody, headers).subscribe((res) => {
+      console.log(res);
+      this.successMessageStatus = 'Success';
+    },
+      ((err) => {
+        this.errorMessageStatus = err;
+      }));
+  }
+
+  deleteRemoteData() {
+    const headers = new HttpHeaders();
+    headers.set('Content-type', 'application/json; charset=UTF-8');
+    this._httpCommonService.delete('posts/1', headers).subscribe((res) => {
+      console.log(res);
+      this.successMessageStatus = 'Success';
+    },
+      ((err) => {
+        this.errorMessageStatus = err;
+      }));
+  }
 }
