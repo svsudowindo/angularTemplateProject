527e270e2a04f993d8c0d3fc32fb3530a9b0a208
sai kumar SKK <sai.korthivada@xcubelabs.com>
2019-01-24 22:07:43 +0530

    Implemented children routes in sub modules

--- src/app/views/auth-views/login/login-routing.module.ts
+++ src/app/views/auth-views/login/login-routing.module.ts
@@ -5,7 +5,12 @@ import { LoginComponent } from './login.component';
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    children: [
+      {
+        path: '',
+        component: LoginComponent
+      }
+    ]
   }
 ];
 
